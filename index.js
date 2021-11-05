e3c9984ee3337109fc4ce4f747999e917eed3f0d
Noa Udler <udlernoa4@gmail.com>
2021-11-04 22:32:39 -0700

    commit tree 0f911dab643069782457c64671faabe4eda4a210

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function superbowlWin(record) {
+  const winningTeamIfAny = record.find((element) => element.result === "W");
 
+  if (winningTeamIfAny !== undefined) {
+    return winningTeamIfAny.year;
+  } else {
+    return undefined;
+  }
+}
